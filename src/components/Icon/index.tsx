744e180b4a9128b4ecfb2d959f815fbba8871aa1
Himanshu-Tokie <himanshu.saha@chicmic.co.in>
2024-05-08 18:30:20 +0530

    icon added

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,9 +1,10 @@
 import { TouchableOpacity } from "react-native";
 
 export default function Icon({icon,width,height,color,action,style}){
+    const iconContainer = style ??[]
     return (
         <>
-             <TouchableOpacity onPress={action}>
+             <TouchableOpacity onPress={action} style={iconContainer}>
             {icon(width, height, color)}
         </TouchableOpacity>
         </>
